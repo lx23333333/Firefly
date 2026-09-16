b10541edf2d42a561c2d37166b69b7f9455fd690
lx23333333 <jtyang1117@163.com>
2026-09-16 16:36:53 +0800

    这是我的博客

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -91,7 +91,7 @@ export const siteConfig: SiteConfig = {
 	favicon: [
 		{
 			// 图标文件路径
-			src: "/src/assets/images/lqxfavicon.png",
+			src: "src/assets/images/lqxfavicon.png",
 			// 可选，指定主题 'light' | 'dark'
 			// theme: "light",
 			// 可选，图标大小
